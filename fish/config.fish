5f4abb46901e02451488105d11080153c52f5121
coderunner007 <alanjo@protonmail.com>
2023-04-07 04:34:31 +0530

    Copied fish config from mac branch

---
+++ fish/config.fish
@@ -0,0 +1,41 @@
+set PATH $HOME/.local/bin $HOME/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin $HOME/.fzf/bin
+set COMMON_ALIASES $HOME/.config/alias
+
+###########
+# Aliases #
+###########
+# source $COMMON_ALIASES/common_aliases.sh
+# source $COMMON_ALIASES/amzn.sh
+
+################
+# Amazon Cards #
+################
+# bash -c "source $HOME/.cardcli_profile"
+# set PATH $PATH /Users/alanjos/aws-cli/bin
+# # Hack to set the node & npm executables in path
+# for dir in $HOME/.nvm/versions/node/*/bin
+#   set PATH $dir $PATH
+# end
+
+################
+# Key bindings #
+################
+function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
+  for mode in default insert visual
+    bind -M $mode -k nul end-of-line accept-autosuggestion
+    bind -M $mode \cp up-or-search
+    bind -M $mode \cn down-or-search
+  end
+end
+hybrid_bindings
+fish_vi_key_bindings
+set -g fish_escape_delay_ms 10
+
+################
+#  Initialize  #
+################
+zoxide init fish | source
+
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
