df9440de09aa45c31fe9b4b97050157c83f509e2
Alan Joseph <ajoseph@coursera.org>
2026-07-01 03:32:02 +0530

    Some more minor changes

--- .config/nvim/lua/plugins/git-diff.lua
+++ .config/nvim/lua/plugins/git-diff.lua
@@ -5,6 +5,6 @@ return {
   keys = {
     { "<leader>gv", "<cmd>DiffviewOpen<CR>",        desc = "Git diff: open working tree" },
     { "<leader>gx", "<cmd>DiffviewClose<CR>",       desc = "Git diff: close" },
-    { "<leader>gV", "<cmd>DiffviewOpen HEAD~1<CR>", desc = "Git diff: last commit" },
+    { "<leader>gV", "<cmd>DiffviewOpen main", desc = "Git diff: main" },
   },
 }
